3e76f20aa158f03e32af3028a3c74310d7d43c0b
Dines Poulsen <dinespoulsen84@gmail.com>
2017-01-26 10:37:15 +0000

    Added test that field can be taken by a player

---
+++ spec/FieldSpec.js
@@ -0,0 +1,14 @@
+describe("Field", function(){
+
+  var myField;
+
+  beforeEach(function(){
+    myField = new Field();
+  });
+
+  it("should be able to be taken by a player", function(){
+    playerX = new Player("X");
+    myField.place(playerX);
+    expect(myField.takenBy()).toEqual("X")
+  });
+});
